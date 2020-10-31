8acbe5c251ccc52d12827df94b2fc93ecf2b406e
codwerk <louis_widdy@live.fr>
2020-11-01 00:17:47 +0100

    integrattion html/css, create method searchMeal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="container" style="height: 100vh;">
+
+        <h1 class="title has-text-link has-text-centered">Cuisiner pour tous</h1>
+
+        <div class="field is-horizontal is-justify-content-center is-align-items-center my-2">
+          
+          <div class="field mb-0 column is-6">
+            <input class="input is-rounded" type="text" placeholder="Entrez un nom de plat" v-model="meal" @input="searchMeal">
+            
+          </div>
+
+          <div class="field has-text-centered">
+               <div class="field-body">
+                <div class="field">
+                  <div class="control">
+                    <button class="button is-link is-rounded">
+                      Choisir un plat au hasard
+                    </button>
+                  </div>
+                </div>
+              </div>
+          </div>
+          
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      meal: null,
+      meals: null
+    }
+  },
+  methods: {
+    searchMeal() {
+      return this.$http.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${this.meal}`)
+            .then( response => {
+              this.meals = response.data.meals;
+              console.log(this.meals);
+            })
+    }
   }
 }
 </script>
 
 <style>
+
+*, ::before, ::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
